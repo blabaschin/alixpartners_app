ccce55eef1d5042f00daa3113ceb5dec96676c65
Ben Labaschin <blabaschin@MacBook-Pro.local>
2022-01-26 17:35:23 -0600

    first commit

---
+++ app/pages/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import boto3
+
+def run():
+    st.header("Welcome to the Workhelix!")
+
+    st.subheader("Useful Links")
+    st.markdown("---")
+    st.markdown("[Workhelix Website](https://workhelix.com/)")
+
